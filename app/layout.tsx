41fd2e504eaa15eb2939ad5d327eea991f4cf797
tilletben <benjamin.tillett@gmail.com>
2025-06-29 08:16:39 +0100

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
-import "@/app/ui/global.css";
-import { inter } from "@/app/ui/fonts";
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Welcome to Acme',
+};
 
 export default function RootLayout({
   children,
@@ -7,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
